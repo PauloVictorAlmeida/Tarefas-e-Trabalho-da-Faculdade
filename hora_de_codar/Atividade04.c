deebba34df5cb00cdf2dc28859f10fc22b8bee5b
Paulo Victor <paulovictor.a.g4@gmail.com>
2025-10-01 14:31:30 -0300

    atividade utilizando if, else if e else
    Aprendendo estruturas de decisão encadeadas

---
+++ hora_de_codar/Atividade04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int idade;
+
+    printf("Digite a sua idade: \n");
+    scanf("%d", &idade);
+
+    if (idade >= 60){
+        printf("Voce é idoso! \n");
+    } else if (idade >= 18 && idade < 60){
+        printf("Voce é adulto! \n");
+    }else if (idade >=12 && idade <18){
+        printf("Voce é adolescente! \n");
+    }else{
+        printf("Voce é uma criança! \n");
+    }
+
+    return 0;
+}
